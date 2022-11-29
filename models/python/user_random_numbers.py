db4ad3c399a246855d8ac3c09e47265540857872
Lorenzo Rubio <104133639+lorenzorubi-db@users.noreply.github.com>
2022-11-29 09:33:49 +0100

    add sample python function

---
+++ models/python/user_random_numbers.py
@@ -0,0 +1,19 @@
+import pyspark.sql.types as T
+import pyspark.sql.functions as F
+import random
+
+
+@F.udf(returnType=T.IntegerType())
+def add_random(x):
+   random_number = random.randint(1,99)
+   return x + random_number
+
+
+def model(dbt, session):
+   dbt.config(materialized="table")
+
+   # DataFrame representing an upstream model
+   users_df = dbt.ref("stg_customers")
+
+   df = users_df.withColumn("user_random_number", add_random(users_df["customer_id"]))
+   return df
