45235204870caae1908df811c075226765051807
Lorenzo Rubio <104133639+lorenzorubi-db@users.noreply.github.com>
2023-08-18 17:25:22 +0200

    cleanup & test submission method

--- models/python/user_random_numbers.py
+++ models/python/user_random_numbers.py
@@ -1,21 +1,26 @@
-# # import pyspark.sql.types as T
-# # import pyspark.sql.functions as F
-# import random
+import random
 
 
-# # @F.udf(returnType=T.IntegerType())
-# def add_random(x):
-#    random_number = random.randint(1,99)
-#    return x + random_number
+## alternative: as UDF, not really needed for now
+#@F.udf(returnType=T.IntegerType())
+def add_random(x):
+   random_number = random.randint(1,99)
+   return x + random_number
 
-import importlib
-
-module = importlib.import_module("app.add_random_udf")
-add_random = getattr(module, "add_random")
+## alternative: as imported package (needs to be installed in the cluster)
+# import importlib
+#
+# module = importlib.import_module("app.add_random_udf")
+# add_random = getattr(module, "add_random")
 
 
 def model(dbt, session):
-   dbt.config(materialized="table")
+   dbt.config(
+      materialized="table",
+      # # uncomment the following for submission under all-purpose cluster
+      # submission_method="all_purpose_cluster",
+      # cluster_id="0812-165614-tibia842"
+   )
 
    # DataFrame representing an upstream model
    users_df = dbt.ref("stg_customers")
